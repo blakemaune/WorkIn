82a998b87b0333f2d21e9bdba173c7f75cf43a36
Blake Maune <bfm7@miami.edu>
2017-05-25 22:20:05 -0500

    First commit

---
+++ gymtrack/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GymtrackConfig(AppConfig):
+    name = 'gymtrack'
---
+++ gymtrack/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.utils import timezone
+from datetime import timedelta
+
+# Create your models here.
+DEFAULT_WORKOUT_ID = 1
+class Exercise(models.Model):
+	NAME_CHOICES = (
+	    ('Arms', (
+	            ('bicurl', 'Biceps Curl'),
+	            ('armpress', 'Shoulder Press'),
+	            ('tricurl', 'Triceps Curl'),
+	        )
+	    ),
+	    ('Legs', (
+	            ('legpress', 'Leg Press'),
+	            ('legcurl', 'Leg Curl'),
+	            ('legext', 'Leg Extension'),
+	            ('abduction', 'Leg Abduction'),
+	            ('adduction', 'Leg Adduction'),
+	        )
+	    ),
+	    ('Chest', (
+	    		('chestpress', 'Chest Press'),
+	    		('chestfly', 'Chest Flys'),
+	    	)
+	    ),
+	    ('abs', 'Abdominal Crunches'),
+	    ('cardio', 'Cardio Exercise'),
+	    ('unknown', 'Unknown'),
+	)
+	name = models.CharField(max_length=100, choices=NAME_CHOICES)
+	max_weight = models.PositiveSmallIntegerField()
+	num_sets = models.PositiveSmallIntegerField()
+	reps_per_set = models.PositiveSmallIntegerField()
+	duration = models.DurationField(blank=True, null=True)
+	workout = models.ForeignKey('Workout', default = DEFAULT_WORKOUT_ID);
+
+	def __str__(self):
+		return self.name
+
+class Gym(models.Model):
+	name = models.CharField(max_length = 50)
+	location = models.CharField(max_length = 100)
+	notes = models.CharField(max_length = 200)
+
+	def __str__(self):
+		return self.name
+
+class Workout(models.Model):
+	gym = models.ForeignKey('Gym', on_delete=models.CASCADE)
+	workout_time = models.DateTimeField(default = timezone.now)
+	# exercises = models.ManyToManyField(Exercise)
+
+	def __str__(self):
+		outStr = self.workout_time.__str__() + " at " + self.gym.name
+		return outStr
